5fae6c22a899aa9ceee453e2bea1200a120dd7dc
Steffen Kahl <steffen.kahl@ue-germany.de>
2020-11-22 19:40:29 +0100

    [ADDED] ModProject

---
+++ src/main/java/com/stevenpaw/awesomeshop/AwesomeShop.java
@@ -0,0 +1,37 @@
+package com.stevenpaw.awesomeshop;
+
+import com.stevenpaw.awesomeshop.setup.Registration;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("awesomeshop")
+public class AwesomeShop
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "awesomeshop";
+
+    public AwesomeShop()
+    {
+
+        Registration.Register();
+
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event)
+    {
+    }
+}
