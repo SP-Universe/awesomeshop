b0b82563e2bdaf346a61bd4dc4b181b3ed4ba6d0
Steffen Kahl <steffen.kahl@ue-germany.de>
2020-11-26 13:12:08 +0100

    [CHANGED] IExampleRecipe

--- src/main/java/com/stevenpaw/awesomeshop/recipes/IExampleRecipe.java
+++ src/main/java/com/stevenpaw/awesomeshop/recipes/IExampleRecipe.java
@@ -1,22 +1,31 @@
 package com.stevenpaw.awesomeshop.recipes;
 
+import javax.annotation.Nonnull;
+
 import com.stevenpaw.awesomeshop.AwesomeShop;
+
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import net.minecraftforge.items.wrapper.RecipeWrapper;
 
-import javax.annotation.Nonnull;
 
-public class IExampleRecipe extends IRecipe<RecipeWrapper> {
+public interface IExampleRecipe extends IRecipe<RecipeWrapper> {
 
     ResourceLocation RECIPE_TYPE_ID = new ResourceLocation(AwesomeShop.MOD_ID, "example");
 
     @Nonnull
     @Override
-    default IRecipeType<?> getType()
-    {
-        return Registry.RECIPE_TYPE.getValueForKey(RECIPE_TYPE_ID);
+    default IRecipeType<?> getType() {
+        return Registry.RECIPE_TYPE.getValueForKey(RECIPE_TYPE_ID).get();
     }
+
+    @Override
+    default boolean canFit(int width, int height) {
+        return false;
+    }
+
+    Ingredient getInput();
 }
